4749d9b2d6e3bd0f6ff96737aeaff640ce43d846
Vladmir Neres <vlade0002@gmail.com>
2022-08-18 21:48:33 -0300

    Adicionando metodo calcularSalario

--- public class Empregado.java
+++ public class Empregado.java
@@ -2,16 +2,48 @@ public class Empregado
 {
     private String nome;
     private int idade;
-    public String getNome (){
-    return this.nome;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calcularSalario()
+    {
+            if (tipo===1)
+            {
+
+                return salario;
+            }
+            else if (tipo ===2 )
+            {
+                return salario + salario*comissao;
+            }
+            else if (tipo === 3)
+            {
+                return salario +bonus;
+            }
+            else
+            {
+                return 0;
+            }
     }
-    public void setNome (String nome){
-    this.nome = nome;
+
+    public String getNome ()
+    {
+        return this.nome;
     }
-    public int getIdade (){
-    return this.idade;
+    
+    public void setNome (String nome)
+    {
+        this.nome = nome;
     }
-    public void setIdade (int idade){
-    this.idade = idade;
+    public int getIdade ()
+    {
+        return this.idade;
+    }
+    
+    public void setIdade (int idade)
+    {
+         this.idade = idade;
     }
 }
